e7c011f0fbd1db8f0d1c46a91438e6329a1451db
Jampier Santiago Moreno Arenas <67984118+jampier-santiago@users.noreply.github.com>
2021-08-27 10:20:12 -0500

    create the section 'title product'

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,6 +10,20 @@ export default function Main() {
     <section className="container-page">
       <Nav />
       <Slider />
+
+      <section className="product-title">
+        <div className="product-title__alert">
+          <span>New release</span>
+        </div>
+        <div className="product-title__title">
+          <h2>MOMENTUM True Wireless 2</h2>
+          <h3>Earbuds that put sound first</h3>
+        </div>
+        <div className="product-title__price">
+          <small>Starting at</small>
+          <h3>$295.95</h3>
+        </div>
+      </section>
     </section>
   );
 }
