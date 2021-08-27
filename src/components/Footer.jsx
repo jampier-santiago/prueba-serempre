90718f60ec19f429a4f89c491fda3bc6c432ac88
Jampier Santiago Moreno Arenas <67984118+jampier-santiago@users.noreply.github.com>
2021-08-27 13:33:57 -0500

    create the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="container-footer">
+      <div className="container-footer__info">
+        <h2>$295.95</h2>
+        <span>Need financing? Learn more </span>
+      </div>
+      <div className="container-footer__button">
+        <span>Buy now</span>
+      </div>
+    </footer>
+  );
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 // Dependecies
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../components/Card";
+import Footer from "../components/Footer";
 
 // Components
 import Nav from "../components/Nav";
@@ -8,91 +9,98 @@ import Slider from "../components/Slider";
 import SliderInfo from "../components/SliderInfo";
 
 export default function Main() {
+  useEffect(() => {
+    document.title = "Serempre";
+  }, []);
+
   return (
-    <section className="container-page">
-      <Nav />
-      <Slider />
+    <>
+      <section className="container-page">
+        <Nav />
+        <Slider />
 
-      <section className="product-title">
-        <div className="product-title__alert">
-          <span>New release</span>
-        </div>
-        <div className="product-title__title">
-          <h2>MOMENTUM True Wireless 2</h2>
-          <h3>Earbuds that put sound first</h3>
-        </div>
-        <div className="product-title__price">
-          <small>Starting at</small>
-          <h3>$295.95</h3>
-        </div>
+        <section className="product-title">
+          <div className="product-title__alert">
+            <span>New release</span>
+          </div>
+          <div className="product-title__title">
+            <h2>MOMENTUM True Wireless 2</h2>
+            <h3>Earbuds that put sound first</h3>
+          </div>
+          <div className="product-title__price">
+            <small>Starting at</small>
+            <h3>$295.95</h3>
+          </div>
+        </section>
+
+        <SliderInfo />
+
+        <section className="container-your-finish">
+          <div className="container-your-finish__title">
+            <h2>Choose your finish.</h2>
+          </div>
+          <div className="container-your-finish__cards">
+            <Card
+              title="Ivory White"
+              text="For the past 75 years, Sennheiser has put sound first. The new MOMENTUM True. "
+            />
+            <Card
+              title="Matte Black"
+              text="Of all of the celestial bodies that capture our attention and fascination as astronomers."
+            />
+          </div>
+        </section>
+
+        <section className="container-add-extended">
+          <div className="container-add-extended__title">
+            <h2>Would you like to add extended.</h2>
+          </div>
+          <div className="container-add-extended__cards">
+            <Card
+              title="2 years coverage"
+              text="For the past 75 years, Sennheiser has put sound first."
+            ></Card>
+            <Card
+              title="3 years coverage"
+              text="For the past 75 years, Sennheiser has put sound first."
+              price="+ $75"
+            ></Card>
+          </div>
+        </section>
+
+        <section className="container-specifications">
+          <div className="container-specifications__title">
+            <h2>Specifications.</h2>
+          </div>
+          <div className="container-specifications__table">
+            <div className="table__row">
+              <h3>Dimensions</h3>
+              <span>76.8 x 43.8 x 34.7 mm (earbuds and charging case)</span>
+            </div>
+            <div className="table__row">
+              <h3>USB Standard</h3>
+              <span>USB-C</span>
+            </div>
+            <div className="table__row">
+              <h3>Power supply</h3>
+              <span>Sennheiser 7mm dynamic driver</span>
+            </div>
+            <div className="table__row">
+              <h3>Frequency response (Microphone)</h3>
+              <span>100 Hz to 10 kHz</span>
+            </div>
+            <div className="table__row">
+              <h3>Frequency response</h3>
+              <span>5 - 21,000 Hz</span>
+            </div>
+            <div className="table__row">
+              <h3>Noise cancellation</h3>
+              <span>Single-Mic ANC per earbud side</span>
+            </div>
+          </div>
+        </section>
       </section>
-
-      <SliderInfo />
-
-      <section className="container-your-finish">
-        <div className="container-your-finish__title">
-          <h2>Choose your finish.</h2>
-        </div>
-        <div className="container-your-finish__cards">
-          <Card
-            title="Ivory White"
-            text="For the past 75 years, Sennheiser has put sound first. The new MOMENTUM True. "
-          />
-          <Card
-            title="Matte Black"
-            text="Of all of the celestial bodies that capture our attention and fascination as astronomers."
-          />
-        </div>
-      </section>
-
-      <section className="container-add-extended">
-        <div className="container-add-extended__title">
-          <h2>Would you like to add extended.</h2>
-        </div>
-        <div className="container-add-extended__cards">
-          <Card
-            title="2 years coverage"
-            text="For the past 75 years, Sennheiser has put sound first."
-          ></Card>
-          <Card
-            title="3 years coverage"
-            text="For the past 75 years, Sennheiser has put sound first."
-            price="+ $75"
-          ></Card>
-        </div>
-      </section>
-
-      <section className="container-specifications">
-        <div className="container-specifications__title">
-          <h2>Specifications.</h2>
-        </div>
-        <div className="container-specifications__table">
-          <div className="table__row">
-            <h3>Dimensions</h3>
-            <span>76.8 x 43.8 x 34.7 mm (earbuds and charging case)</span>
-          </div>
-          <div className="table__row">
-            <h3>USB Standard</h3>
-            <span>USB-C</span>
-          </div>
-          <div className="table__row">
-            <h3>Power supply</h3>
-            <span>Sennheiser 7mm dynamic driver</span>
-          </div>
-          <div className="table__row">
-            <h3>Frequency response (Microphone)</h3>
-            <span>100 Hz to 10 kHz</span>
-          </div>
-          <div className="table__row">
-            <h3>Frequency response</h3>
-            <span>5 - 21,000 Hz</span>
-          </div>
-          <div className="table__row">
-            <h3>Noise cancellation</h3>
-            <span>Single-Mic ANC per earbud side</span>
-          </div>
-        </div>
-      </section>
-    </section>
+      <Footer />
+    </>
   );
 }
