709e9cd555ec2e432159a04802789d74f79002c8
Jampier Santiago Moreno Arenas <67984118+jampier-santiago@users.noreply.github.com>
2021-08-27 09:24:01 -0500

    init project

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+// Dependencies
+import React from "react";
+
+// Assets
+import logo from "../assets/images/logo.png";
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <img src={logo} alt="Logo de Serempre" />
+    </div>
+  );
+}
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+// Dependecies
+import React from "react";
+
+// Components
+import Nav from "../components/Nav";
+
+export default function Main() {
+  return (
+    <section className="container-page">
+      <Nav />
+    </section>
+  );
+}
